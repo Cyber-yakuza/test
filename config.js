7f514cb462189a3aa0f5d557eee2e1cfb8c60d1b
Cyber yakuza <surathaloka218@gmail.com>
2025-03-16 10:05:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "𝐒𝐔𝐋𝐀-𝐌𝐃=SEx0hRiA#sqfepXo-ka7gTAQg2AlCfXyKQKmCNAza8-G18hht5bA",
-MONGODB: process.env.MONGODB || "mongodb+srv://darknero:@Sldarknero123@cluster0.lwnqi.mongodb.net/",
+MONGODB: process.env.MONGODB || "mongodb://mongo:UMGjhhQcSaRTiirgbdcNsjLfXZVNesZe@yamanote.proxy.rlwy.net:18474",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/4g2tYcsx/1387.jpg",
 BOT_NAME: process.env.BOT_NAME || "𝐒𝐔𝐋𝐀-𝐌𝐃",
 LANG: process.env.BOT_LANG || 'EN' ,
